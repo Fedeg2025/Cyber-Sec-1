1888de1dd265e8651910c651d7317e93faaa9449
ROCA <rcamenen@gmail.com>
2026-03-17 08:57:51 +0100

    suppression ligne pour test

--- index.php
+++ index.php
@@ -8,14 +8,5 @@ session_start();
 // Chargement de la config (RACINE, DB, constantes)
 require __DIR__ . "/Config/config.php";
 
-// Chargement des models
-require RACINE . "/Models/database.php";
-require RACINE . "/Models/candidat.php";
-require RACINE . "/Models/questions.php";
-require RACINE . "/Models/session.php";
-require RACINE . "/Models/rh_user.php";
-
 // Chargement et appel du routeur
 require RACINE . "/Config/route.php";
-$route = new Route();
-$route->router();
